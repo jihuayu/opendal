5fa24d6daadf4ce2791bc7d2859f675f18c10a96
Xuanwo <Xuanwo@users.noreply.github.com>
2023-05-16 14:15:06 +0000

    deploy: cbaf11411315aa5fea07acc1437569ee93c711ec

--- docs/rust/opendal/ops/sidebar-items.js
+++ docs/rust/opendal/ops/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"enum":["BatchOperation","PresignOperation"],"struct":["OpBatch","OpCopy","OpCreateDir","OpDelete","OpList","OpPresign","OpRead","OpRename","OpStat","OpWrite"]};
+window.SIDEBAR_ITEMS = {"enum":["BatchOperation","PresignOperation"],"struct":["OpAppend","OpBatch","OpCopy","OpCreateDir","OpDelete","OpList","OpPresign","OpRead","OpRename","OpStat","OpWrite"]};
