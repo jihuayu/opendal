597a506056447560d4d6d49229d874a2675f1471
Xuanwo <Xuanwo@users.noreply.github.com>
2023-05-24 20:09:23 +0000

    deploy: fd708e251f096ce280d64d0a5c90245409007080

--- docs/rust/opendal/operator_futures/sidebar-items.js
+++ docs/rust/opendal/operator_futures/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"struct":["FutureAppend","FutureAppender","FutureRead","FutureStat","FutureWrite"]};
+window.SIDEBAR_ITEMS = {"struct":["FutureAppend","FutureAppender","FutureRead","FutureReader","FutureStat","FutureWrite"]};
