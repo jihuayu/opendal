39e441d49f6c57fb3baf583abbaaf00d2e9bff4b
Xuanwo <Xuanwo@users.noreply.github.com>
2023-05-31 09:18:31 +0000

    deploy: 1adbb54579701f740869ec606b60df5ff6ad41ea

---
+++ docs/c/search/classes_0.js
@@ -0,0 +1,10 @@
+var searchData=
+[
+  ['opendal_5fbytes_45',['opendal_bytes',['../structopendal__bytes.html',1,'']]],
+  ['opendal_5fmetadata_46',['opendal_metadata',['../structopendal__metadata.html',1,'']]],
+  ['opendal_5foperator_5foptions_47',['opendal_operator_options',['../structopendal__operator__options.html',1,'']]],
+  ['opendal_5foperator_5fptr_48',['opendal_operator_ptr',['../structopendal__operator__ptr.html',1,'']]],
+  ['opendal_5fresult_5fis_5fexist_49',['opendal_result_is_exist',['../structopendal__result__is__exist.html',1,'']]],
+  ['opendal_5fresult_5fread_50',['opendal_result_read',['../structopendal__result__read.html',1,'']]],
+  ['opendal_5fresult_5fstat_51',['opendal_result_stat',['../structopendal__result__stat.html',1,'']]]
+];
