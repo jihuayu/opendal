05a0f7cde937f138e43746f886a18842463d0559
Xuanwo <Xuanwo@users.noreply.github.com>
2023-06-18 14:36:17 +0000

    deploy: 800efa93d49bbfea367256e76a0b66324c57ec34

--- docs/c/search/functions_0.js
+++ docs/c/search/functions_0.js
@@ -5,14 +5,14 @@ var searchData=
   ['opendal_5fmetadata_5ffree_57',['opendal_metadata_free',['../opendal_8h.html#a8edd7cec07ae7051e8363da44aa168e6',1,'opendal.h']]],
   ['opendal_5fmetadata_5fis_5fdir_58',['opendal_metadata_is_dir',['../opendal_8h.html#ac8cb949fb37f83dfcbe78976eaa29409',1,'opendal.h']]],
   ['opendal_5fmetadata_5fis_5ffile_59',['opendal_metadata_is_file',['../opendal_8h.html#a8900dddbbce980c3323cedd4f30c33ad',1,'opendal.h']]],
-  ['opendal_5foperator_5fblocking_5fdelete_60',['opendal_operator_blocking_delete',['../opendal_8h.html#a9eeffb62f94b651159ecaa00a9fbbcad',1,'opendal.h']]],
-  ['opendal_5foperator_5fblocking_5fread_61',['opendal_operator_blocking_read',['../opendal_8h.html#ad1d0f0b4ee604bbf7d226cd5db81471d',1,'opendal.h']]],
-  ['opendal_5foperator_5fblocking_5fwrite_62',['opendal_operator_blocking_write',['../opendal_8h.html#a44f96c4a5c9092b47fc2c43ee00526ca',1,'opendal.h']]],
-  ['opendal_5foperator_5ffree_63',['opendal_operator_free',['../opendal_8h.html#a51db6f1e3b4930990ec30f81e7e96ad1',1,'opendal.h']]],
-  ['opendal_5foperator_5fis_5fexist_64',['opendal_operator_is_exist',['../opendal_8h.html#a391725f337d0a0897c95227501f3b188',1,'opendal.h']]],
-  ['opendal_5foperator_5fnew_65',['opendal_operator_new',['../opendal_8h.html#a2af3e11594601424556c8465e2654a45',1,'opendal.h']]],
-  ['opendal_5foperator_5foptions_5ffree_66',['opendal_operator_options_free',['../opendal_8h.html#ae87a9921870d96d1fd23ccf1f7b51c97',1,'opendal.h']]],
-  ['opendal_5foperator_5foptions_5fnew_67',['opendal_operator_options_new',['../opendal_8h.html#a5fc487d22612b761b882da5605891a57',1,'opendal.h']]],
+  ['opendal_5foperator_5fblocking_5fdelete_60',['opendal_operator_blocking_delete',['../opendal_8h.html#ab339b3a262d3b508030a966000833145',1,'opendal.h']]],
+  ['opendal_5foperator_5fblocking_5fread_61',['opendal_operator_blocking_read',['../opendal_8h.html#ae8fc63885af34e53afbb075dfed39732',1,'opendal.h']]],
+  ['opendal_5foperator_5fblocking_5fwrite_62',['opendal_operator_blocking_write',['../opendal_8h.html#a7606349344f0cafa421f8ac71665a2f7',1,'opendal.h']]],
+  ['opendal_5foperator_5ffree_63',['opendal_operator_free',['../opendal_8h.html#a3e038035b4339e19f24e1aa8ef678def',1,'opendal.h']]],
+  ['opendal_5foperator_5fis_5fexist_64',['opendal_operator_is_exist',['../opendal_8h.html#ae5a3bf403cdf78d5c5b1fa9f623f0d80',1,'opendal.h']]],
+  ['opendal_5foperator_5fnew_65',['opendal_operator_new',['../opendal_8h.html#a24a2ccd08c1862ebf680104f3ba0d0e8',1,'opendal.h']]],
+  ['opendal_5foperator_5foptions_5ffree_66',['opendal_operator_options_free',['../opendal_8h.html#a1d65b173b087b2abca126de2e951b412',1,'opendal.h']]],
+  ['opendal_5foperator_5foptions_5fnew_67',['opendal_operator_options_new',['../opendal_8h.html#a67b1457f2b1258551552dc383840f288',1,'opendal.h']]],
   ['opendal_5foperator_5foptions_5fset_68',['opendal_operator_options_set',['../opendal_8h.html#af408af922875eb26c25cef4ddfdede02',1,'opendal.h']]],
-  ['opendal_5foperator_5fstat_69',['opendal_operator_stat',['../opendal_8h.html#a410830e04f6cc10a3aaaad7bb183b2f6',1,'opendal.h']]]
+  ['opendal_5foperator_5fstat_69',['opendal_operator_stat',['../opendal_8h.html#aefa1285911f7cf00837e69ba29d2efd3',1,'opendal.h']]]
 ];
