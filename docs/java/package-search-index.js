f2eac3a520f6638d03c1b001f7cc35953aaa3980
Xuanwo <Xuanwo@users.noreply.github.com>
2023-11-02 12:41:25 +0000

    deploy: 27b2a16f104a825cfaf4a33fb5b5e3da7d335123

--- docs/java/package-search-index.js
+++ docs/java/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"org.apache.opendal"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"org.apache.opendal"},{"l":"org.apache.opendal.layer"}];updateSearchResults();
