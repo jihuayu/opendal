71d1b0ba80ff9d0d0eed959315d81ddb7d74e236
jihuayu <jihuayu123@gmail.com>
2026-09-15 11:18:29 +0800

    fix(java): avoid unnecessary input stream reads

--- bindings/java/src/main/java/org/apache/opendal/OperatorInputStream.java
+++ bindings/java/src/main/java/org/apache/opendal/OperatorInputStream.java
@@ -60,7 +60,7 @@ public class OperatorInputStream extends InputStream {
         if (reader.isDisposed()) {
             throw new IllegalStateException("OperatorInputStream is closed");
         }
-        if (bytes != null && offset >= bytes.length) {
+        while (bytes != null && offset >= bytes.length) {
             bytes = readNextBytes(reader.nativeHandle);
             offset = 0;
         }
@@ -85,7 +85,7 @@ public class OperatorInputStream extends InputStream {
         }
         int read = 0;
         while (len > 0) {
-            if (bytes != null && offset >= bytes.length) {
+            while (bytes != null && offset >= bytes.length) {
                 bytes = readNextBytes(reader.nativeHandle);
                 offset = 0;
             }
@@ -102,17 +102,13 @@ public class OperatorInputStream extends InputStream {
             len -= n;
         }
 
-        if (bytes != null && offset >= bytes.length) {
-            bytes = readNextBytes(reader.nativeHandle);
-            offset = 0;
-        }
-
-        return bytes != null ? read : (read != 0 ? read : -1);
+        return read;
     }
 
     @Override
     public synchronized void close() {
         reader.close();
+        bytes = null;
     }
 
     private static native void disposeReader(long reader);
